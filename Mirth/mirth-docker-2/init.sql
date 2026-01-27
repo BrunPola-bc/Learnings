573a35e8ced8d60c5d7e17ae8ee38b9967eaaef4
Bruno Polančec <bruno.polancec@bytecraft.eu>
2026-01-27 11:23:17 +0100

    Testing Mirth in Docker

---
+++ Mirth/mirth-docker-2/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE test_table (
+  id SERIAL PRIMARY KEY,
+  message VARCHAR(255)
+);
+
+INSERT INTO test_table(message)
+VALUES ('First message from init.sql');
+
+CREATE TABLE counters (
+  name VARCHAR(50) PRIMARY KEY,
+  value INT NOT NULL
+);
+
+INSERT INTO counters(name, value)
+VALUES ('db_writer_channel_counter', 0);
---
+++ Mirth/mirth-tests/index.js
@@ -0,0 +1,8 @@
+let arr1 = [1, 2, 3];
+let arr2 = [101, 102, 103];
+
+let arr3 = [arr1, 50, arr2];
+let arr4 = [...arr1, 50, ...arr2];
+
+console.log(arr3);
+console.log(arr4);
