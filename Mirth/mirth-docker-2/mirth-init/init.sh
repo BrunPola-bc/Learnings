74eac169f9750de2cc75d601bad351edc8e13ec8
Bruno Polančec <bruno.polancec@bytecraft.eu>
2026-01-28 16:01:52 +0100

    Initialization of mirth server

---
+++ Mirth/mirth-docker-2/mirth-init/init.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+set -e
+
+# ----- CONFIGURATION -----
+MIRTH_URL="https://mc:8443"
+INITIALIZATION_MARKER="/opt/connect/appdata/.initialized"
+USER=$(cat /run/secrets/mirth_admin_user)
+PASS=$(cat /run/secrets/mirth_admin_pass)
+
+# -------------------------
+
+# Check marker file
+if [ -f "$INITIALIZATION_MARKER" ]; then
+  echo "Server previously initialized. Exiting."
+  exit 0
+fi
+
+echo "Waiting for Mirth to be ready..."
+
+# BusyBox-compatible wait loop
+while true; do
+    HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" \
+        -u "$USER:$PASS" \
+        -H "Accept: application/xml" \
+        -H "X-Requested-With: OpenAPI" \
+        "$MIRTH_URL/api/server/status" -k || echo "000")
+
+    [ "$HTTP_CODE" -eq 200 ] && break
+    sleep 3
+done
+
+echo "Mirth is ready!"
+
+# Restore server configuration
+HTTP_RESPONSE=$(curl -k -s -o /tmp/mirth_response.xml -w "%{http_code}" \
+  -u "$USER:$PASS" \
+  -H "Content-Type: application/xml" \
+  -H "X-Requested-With: OpenAPI" \
+  -X PUT \
+  --data-binary @/mirth-init/server-config-backup.xml \
+  "$MIRTH_URL/api/server/configuration")
+
+if [ "$HTTP_RESPONSE" -ge 200 ] && [ "$HTTP_RESPONSE" -lt 300 ]; then
+  echo "Configuration restored successfully (HTTP $HTTP_RESPONSE)"
+else
+  echo "ERROR restoring configuration (HTTP $HTTP_RESPONSE)"
+  cat /tmp/mirth_response.xml
+  exit 1
+fi
+
+# Create marker file
+touch "$INITIALIZATION_MARKER"
+
+echo "Initialization complete."
